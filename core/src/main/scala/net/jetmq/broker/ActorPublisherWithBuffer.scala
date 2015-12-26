68e205180fb43b0e3abe1ef43e67cf63c854bd4a
Vitaly Baum <vitaly.baum@gmail.com>
2015-12-26 15:49:42 +0100

    TCP rewritten to Streams

---
+++ core/src/main/scala/net/jetmq/broker/ActorPublisherWithBuffer.scala
@@ -0,0 +1,33 @@
+package net.jetmq.broker
+
+import akka.stream.actor.ActorPublisher
+
+import scala.annotation.tailrec
+
+abstract class ActorPublisherWithBuffer[T] extends ActorPublisher[T] {
+  var buffer = Vector.empty[T]
+
+  def onNextBuffered(envelope: T): Unit = {
+
+    if (buffer.isEmpty && totalDemand > 0) {
+      onNext(envelope)
+    }
+    else {
+      buffer :+= envelope
+    }
+  }
+
+  @tailrec final def deliverBuffer(): Unit =
+    if (totalDemand > 0) {
+      if (totalDemand <= Int.MaxValue) {
+        val (use, keep) = buffer.splitAt(totalDemand.toInt)
+        buffer = keep
+        use foreach onNext
+      } else {
+        val (use, keep) = buffer.splitAt(Int.MaxValue)
+        buffer = keep
+        use foreach onNext
+        deliverBuffer()
+      }
+    }
+}
---
+++ core/src/main/scala/net/jetmq/broker/BrokerServer.scala
@@ -0,0 +1,71 @@
+package net.jetmq.broker
+
+import akka.actor._
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.IncomingConnection
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.ws.{BinaryMessage, UpgradeToWebsocket}
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
+import akka.stream.ActorMaterializer
+import akka.stream.actor.ActorPublisher
+import akka.stream.scaladsl.{Sink, Source, Tcp}
+import akka.util.ByteString
+
+
+object BrokerServer {
+
+
+  def run(system: ActorSystem) = {
+
+    val bus = system.actorOf(Props[EventBusActor], name = "event-bus")
+    val sessions = system.actorOf(Props(new SessionsManagerActor(bus)), name = "sessions")
+
+    implicit val materializer = ActorMaterializer()(system)
+
+    def requestHadler(connection: IncomingConnection, r: HttpRequest): HttpResponse = {
+      r match {
+        case req@HttpRequest(GET, Uri.Path("/mqtt"), _, _, _) =>
+          req.header[UpgradeToWebsocket] match {
+            case Some(upgrade) => {
+
+              val handler = system.actorOf(Props(new WsConnectionActor(sessions)).withMailbox("priority-dispatcher"),
+                connection.remoteAddress.getHostString() + ":" + connection.remoteAddress.getPort())
+
+              upgrade.handleMessagesWithSinkSource(
+                Sink.foreach(handler ! _),
+                Source(ActorPublisher[BinaryMessage](handler))
+              )
+            }
+            case None => HttpResponse(400, entity = "Not a valid websocket request!")
+          }
+        case _: HttpRequest => HttpResponse(404, entity = "Unknown resource!")
+
+      }
+    }
+
+    Http(system).bind(interface = "0.0.0.0", port = 8080)
+      .to(Sink.foreach { connection =>
+        println("Accepted new connection from " + connection.remoteAddress)
+
+        val handler: (HttpRequest) => HttpResponse = r => requestHadler(connection, r)
+        connection.handleWithSyncHandler(handler)
+      })
+      .run()
+
+
+    Tcp(system).bind(interface = "0.0.0.0", port = 1883)
+      .to(Sink.foreach { connection =>
+
+        println("Accepted new connection from " + connection.remoteAddress)
+
+        val handler = system.actorOf(Props(new TcpConnectionActor(sessions)).withMailbox("priority-dispatcher"),
+                connection.remoteAddress.getHostString + ":" + connection.remoteAddress.getPort)
+
+        val publisher = ActorPublisher[ByteString](handler)
+
+        Source(publisher).via(connection.flow).to(Sink.foreach(handler ! _)).run()
+
+      })
+      .run()
+  }
+}
