a23d1c5912fe43b94ce0ef1bacc9536722fdcf52
Vitaly Baum <vitaly.baum@gmail.com>
2016-01-16 11:06:32 +0100

    added missed web sockets implementation

--- core/src/main/scala/net/jetmq/broker/BrokerServer.scala
+++ core/src/main/scala/net/jetmq/broker/BrokerServer.scala
@@ -1,6 +1,13 @@
 package net.jetmq.broker
 
-import akka.actor.{ActorSystem, Props}
+import java.net.InetSocketAddress
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.IncomingConnection
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.ws.{BinaryMessage, Message, UpgradeToWebsocket}
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
 import akka.stream.ActorMaterializer
 import akka.stream.actor.{ActorPublisher, ActorSubscriber}
 import akka.stream.scaladsl.{Flow, Sink, Source, Tcp}
@@ -22,32 +29,85 @@ object BrokerServer {
 
         println("Accepted new connection from " + connection.remoteAddress)
 
-        val connectionActor = system.actorOf(Props(new MqttConnectionActor(sessions)))
-
-        def logger[T](prefix: String) = Flow[T].map(msg => {
-          println(s"$prefix > ${msg}")
-          msg
-        })
-
-        val publisher = ActorPublisher[Packet](connectionActor)
-
-        val source = Source(publisher)
-          .via(logger("mqtt < "))
-          .map(PacketsHelper.encode(_).require)
-          .map(_.toByteArray)
-          .map(ByteString(_))
-
-        val subscriber = ActorSubscriber[Packet](connectionActor)
-
-        val sink = Flow[ByteString]
-          .via(logger("row > "))
-          .map(x => x.toBitVector)
-          .mapConcat(PacketsHelper.decode(_))
-          .via(logger("mqtt > "))
-          .to(Sink(subscriber))
+        val (sink, source) = ConstructSinkAndSource(system, sessions, connection.remoteAddress)
 
         connection handleWith Flow.fromSinkAndSource(sink, source)
       })
-      .run().onComplete(println)
+        .run()
+        .onComplete(x => {
+          println(x)
+        })
+
+
+        def requestHadler(connection: IncomingConnection, r: HttpRequest): HttpResponse = {
+          r match {
+            case req@HttpRequest(GET, Uri.Path("/mqtt"), _, _, _) =>
+              req.header[UpgradeToWebsocket] match {
+                case Some(upgrade) => {
+
+
+                  val (sinkByteString, sourceByteString) = ConstructSinkAndSource(system, sessions, connection.remoteAddress)
+
+                  val sink = Flow[Message].map {
+                    case BinaryMessage.Strict(d) => d
+                    case _ => ByteString()
+                  }.to(sinkByteString)
+
+                  val source = sourceByteString.map(BinaryMessage(_))
+
+                  upgrade.handleMessagesWithSinkSource(sink, source)
+                }
+                    case None => HttpResponse(400, entity = "Not a valid websocket request!")
+              }
+                    case _: HttpRequest => HttpResponse(404, entity = "Unknown resource!")
+
+          }
+        }
+
+        println("Http binding...")
+        Http(system).bind(interface = "0.0.0.0", port = 8080)
+          .to(Sink.foreach { connection =>
+            println("Accepted new connection from " + connection.remoteAddress)
+
+            val handler: (HttpRequest) => HttpResponse = r => requestHadler(connection, r)
+            connection.handleWithSyncHandler(handler)
+          })
+            .run()
+            .onComplete(x => {
+              println(x)
+            })
+
   }
+
+  def logger[T](prefix: String) = Flow[T].map(msg => {
+
+    println(s"$prefix ${msg}")
+    msg
+  })
+
+  def ConstructSinkAndSource(system: ActorSystem, sessions: ActorRef, remoteAddress: InetSocketAddress): (Sink[ByteString, Unit], Source[ByteString, Unit]) = {
+
+    val name = remoteAddress.getHostString() + ":" + remoteAddress.getPort()
+
+    val connectionActor = system.actorOf(Props(new MqttConnectionActor(sessions)), name)
+
+    val publisher = ActorPublisher[Packet](connectionActor)
+
+    val source = Source(publisher)
+      .via(logger("mqtt < "))
+      .map(PacketsHelper.encode(_).require)
+      .map(_.toByteArray)
+      .map(ByteString(_))
+
+      val subscriber = ActorSubscriber[Packet](connectionActor)
+
+      val sink = Flow[ByteString]
+        .map(x => x.toBitVector)
+        .mapConcat(PacketsHelper.decode(_))
+        .via(logger("mqtt > "))
+        .to(Sink(subscriber))
+
+        (sink, source)
+  }
+
 }
